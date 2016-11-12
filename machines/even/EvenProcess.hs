3852cc6a9511fd19f03c1bba622f329cd4ea028c
stites <sam@stites.io>
2016-11-12 15:14:40 -0500

    add header

--- machines/even/EvenProcess.hs
+++ machines/even/EvenProcess.hs
@@ -1,3 +1,24 @@
+---------------------------------------------------------------------------------
+-- |
+-- Program which produces a long data-file simulating the strictly sofic system
+-- known as the even process.
+--
+-- The process produces strings of ones and zeros, subject to the constraints
+-- that ones are produced in blocks of even length.  Thus, in state A, we may
+-- produce a 0, which leads back to A, or we may produce a one, which leads to B.
+-- In B we must produce a 1 and return to A.
+--
+-- The probability of emitting a 0 in state A is 0.5
+--
+-- For the even process:
+--  + We always begin in state A.
+--  + This takes, as input, the number of steps to simulate.
+--  + The output symbols are unspaced by default.
+--  + Output is to EP_timeseq
+--  + [ ] Should also output a machine file, EP_machine
+--  + To be used in conjunction with CSSR
+--
+---------------------------------------------------------------------------------
 module EvenProcess where
 
 import Data.Proxy
